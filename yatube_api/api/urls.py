2c58ca85c500fe2c832a5dfd01fd182ccfa86ba2
Alexander <precipitance@gmail.com>
2021-12-07 23:21:11 +0300

    rev2

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -14,14 +14,16 @@ router.register(r'^posts/(?P<post_id>\d+)/comments',
                 CommentViewSet, basename='comments')
 router.register('follow', FollowViewSet, basename='follow')
 
-v1_patterns = [
-    path('jwt/create/',
+jwt_patterns = [
+    path('create/',
          TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('jwt/verify/', TokenVerifyView.as_view(), name='token_refresh'),
-    path('', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('verify/', TokenVerifyView.as_view(), name='token_refresh'),
+    
 ]
+
 urlpatterns = [
-    path('v1/', include(v1_patterns)),
+    path('v1/jwt/', include(jwt_patterns)),
+    path('v1/', include(router.urls)),
+    path('v1/api-token-auth/', views.obtain_auth_token),
 ]
